fa7a7e7e72c68b3b51c7e3ae76806ab5f56f1517
Bilal2912 <bilalsani2912@gmail.com>
2024-05-28 12:54:10 +0530

    chnages

--- services/google/GoogleService.js
+++ services/google/GoogleService.js
@@ -161,15 +161,16 @@ class GoogleService {
   async getOneRecord(recordId) {
     const record = await DummyData.findOne({_id: recordId, status: 'duplicate'}).populate("mostSimilarDocument")
     const keys = Object.keys(record.userData)
-    let obj = {}
+    let objArray = []
     for(const key of keys) {
-      obj[key] = {
+      objArray.push({
+        columnKey: key,
         document: record.userData[key],
         duplicate: record.mostSimilarDocument.userData[key]
-      }
+      })
     }
     return {
-      comparison: obj,
+      comparison: objArray,
       similarityScore: record.similarityScore
     }
   }
